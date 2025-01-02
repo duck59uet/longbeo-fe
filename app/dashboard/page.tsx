1fff8d2318bfcb101798eee11e2d25cefee280f8
duck59uet <ducnv1911@gmail.com>
2025-01-02 15:43:36 +0700

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
+import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Dashboard() {
-  const session = localStorage.getItem('accessToken');
+  const session = await auth();
 
-  if (!session) {
+  if (!session?.user) {
     return redirect('/');
   } else {
     redirect('/dashboard/overview');
---
+++ auth.config.ts
@@ -0,0 +1,51 @@
+import { NextAuthConfig, Session } from 'next-auth';
+import CredentialProvider from 'next-auth/providers/credentials';
+import { loginUser } from './services/login';
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+  }
+}
+const authConfig = {
+  providers: [
+    CredentialProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        try {
+          const user = await loginUser({
+            username: credentials?.username as string,
+            password: credentials?.password as string
+          });
+          if (user) {
+            return { ...user, token: user.token };
+          } else {
+            return null;
+          }
+        } catch (error) {
+          return null;
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = (user as any).token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    }
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/auth/signin'
+  }
+} satisfies NextAuthConfig;
+export default authConfig;
