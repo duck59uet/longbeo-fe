02a6f5f03963ff21a3f2687de00856a54b6b8383
duck59uet <ducnv1911@gmail.com>
2025-04-08 09:59:06 +0700

    fix

--- app/topup/page.tsx
+++ app/topup/page.tsx
@@ -1,12 +1,7 @@
-import { searchParamsCache } from '@/lib/searchparams';
-import { SearchParams } from 'nuqs/parsers';
 import React from 'react';
 import TopupPage from './_components/profile-create-form';
 import DashboardLayout from '../wrapperLayout';
 
-type pageProps = {
-  searchParams: SearchParams;
-};
 
 export const metadata = {
   title: 'Nạp tiền tài khoản'
